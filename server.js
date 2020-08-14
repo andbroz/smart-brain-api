59e1657d0dd069cd2855e943b51de74fff5e805a
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-08-14 21:20:17 +0200

    cors

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt'); //https://www.npmjs.com/package/bcrypt
+const cors = require('cors'); //https://www.npmjs.com/package/cors
 
 const app = express();
 
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
-const port = 3000;
+const port = 3001;
+const saltRounds = 10;
 
 const database = {
 	users: [
@@ -14,7 +18,7 @@ const database = {
 			id: '123',
 			name: 'John',
 			email: 'john@gmail.com',
-			password: 'cookies',
+			password: '$2b$10$.qZhsnk2WfGJcUXFHbzXNOaFamtclYSCY9ShFpQsa5A94PxHzekDS', //cookies
 			entries: 0,
 			joined: new Date(),
 		},
@@ -22,9 +26,16 @@ const database = {
 			id: '124',
 			name: 'Sally',
 			email: 'sally@gmail.com',
-			password: 'bananas',
+			password: '$2b$10$6ee7KXVvAl88ag46VCuYHepuKtnzHUSf8IsYEYR43jLaTTWtZMNbq', //bananas
 			entries: 0,
 			joined: new Date(),
+		},
+	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'john@gmail.com',
 		},
 	],
 };
@@ -35,33 +46,43 @@ app.get('/', (req, res) => {
 });
 
 // signin route
-app.post('/signin', (req, res) => {
+app.post('/signin', async (req, res) => {
 	const { email, password } = req.body;
 
-	if (
-		email === database.users[0].email &&
-		password === database.users[0].password
-	) {
-		res.json('success');
-	}
+	const user = database.users.find(user => user.email === email);
+	try {
+		const match = await bcrypt.compare(password, user.password);
 
-	res.status(404).json('error logging in');
+		if (match) {
+			res.json('success');
+		} else {
+			res.status(404).json('error logging in');
+		}
+	} catch (err) {
+		res.status(404).json('some error occured during logging in');
+	}
 });
 
 // register new user route
 app.post('/register', (req, res) => {
 	const { email, password, name } = req.body;
 
-	const userId = database.users.push({
-		id: String(Number(database.users[database.users.length - 1].id) + 1),
-		name: name,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date(),
+	bcrypt.hash(password, saltRounds, function (err, hashPassword) {
+		if (!err) {
+			const userId = database.users.push({
+				id: String(Number(database.users[database.users.length - 1].id) + 1),
+				name: name,
+				email: email,
+				password: hashPassword,
+				entries: 0,
+				joined: new Date(),
+			});
+			res.json(database.users[userId - 1]);
+		} else {
+			console.log('crating hash failed: ', err);
+			res.json('some error occured');
+		}
 	});
-
-	res.json(database.users[userId - 1]);
 });
 
 // user profile route
